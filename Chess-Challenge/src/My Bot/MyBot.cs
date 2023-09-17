e8460891ca3962f22331ea8162e08fbf689aa2a9
Edward-789 <142745633+Edward-789@users.noreply.github.com>
2023-09-17 10:39:22 +0100

    -4 tokens

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -10,23 +10,19 @@ using System.Linq;
 
         // Tyrants PSTs
         // why is tyrant so good at C#?
-        private readonly int[] PieceValues = { 82, 337, 365, 477, 1025, 0, // Middlegame
-                                                94, 281, 297, 512, 936, 0}, // Endgame      
-                                scores = new int [218];
-        private readonly int[][] UnpackedPestoTables;   
+        private readonly int[] scores = new int [218];
 
-        int piece;  
-        public MyBot()
-        {
-            UnpackedPestoTables = new[] {  63746705523041458768562654720m, 71818693703096985528394040064m, 75532537544690978830456252672m, 75536154932036771593352371712m, 76774085526445040292133284352m, 3110608541636285947269332480m, 936945638387574698250991104m, 75531285965747665584902616832m,77047302762000299964198997571m, 3730792265775293618620982364m, 3121489077029470166123295018m, 3747712412930601838683035969m, 3763381335243474116535455791m, 8067176012614548496052660822m, 4977175895537975520060507415m, 2475894077091727551177487608m,2458978764687427073924784380m, 3718684080556872886692423941m, 4959037324412353051075877138m, 3135972447545098299460234261m, 4371494653131335197311645996m, 9624249097030609585804826662m, 9301461106541282841985626641m, 2793818196182115168911564530m, 77683174186957799541255830262m, 4660418590176711545920359433m, 4971145620211324499469864196m, 5608211711321183125202150414m, 5617883191736004891949734160m, 7150801075091790966455611144m, 5619082524459738931006868492m, 649197923531967450704711664m,  75809334407291469990832437230m, 78322691297526401047122740223m, 4348529951871323093202439165m, 4990460191572192980035045640m, 5597312470813537077508379404m, 4980755617409140165251173636m, 1890741055734852330174483975m, 76772801025035254361275759599m,75502243563200070682362835182m, 78896921543467230670583692029m, 2489164206166677455700101373m, 4338830174078735659125311481m, 4960199192571758553533648130m, 3420013420025511569771334658m, 1557077491473974933188251927m, 77376040767919248347203368440m,  73949978050619586491881614568m, 77043619187199676893167803647m, 1212557245150259869494540530m, 3081561358716686153294085872m, 3392217589357453836837847030m, 1219782446916489227407330320m, 78580145051212187267589731866m, 75798434925965430405537592305m,68369566912511282590874449920m, 72396532057599326246617936384m, 75186737388538008131054524416m, 77027917484951889231108827392m, 73655004947793353634062267392m, 76417372019396591550492896512m, 74568981255592060493492515584m, 70529879645288096380279255040m}
+        static int piece;  
+
+        private readonly int[][] UnpackedPestoTables = new[] {  63746705523041458768562654720m, 71818693703096985528394040064m, 75532537544690978830456252672m, 75536154932036771593352371712m, 76774085526445040292133284352m, 3110608541636285947269332480m, 936945638387574698250991104m, 75531285965747665584902616832m,77047302762000299964198997571m, 3730792265775293618620982364m, 3121489077029470166123295018m, 3747712412930601838683035969m, 3763381335243474116535455791m, 8067176012614548496052660822m, 4977175895537975520060507415m, 2475894077091727551177487608m,2458978764687427073924784380m, 3718684080556872886692423941m, 4959037324412353051075877138m, 3135972447545098299460234261m, 4371494653131335197311645996m, 9624249097030609585804826662m, 9301461106541282841985626641m, 2793818196182115168911564530m, 77683174186957799541255830262m, 4660418590176711545920359433m, 4971145620211324499469864196m, 5608211711321183125202150414m, 5617883191736004891949734160m, 7150801075091790966455611144m, 5619082524459738931006868492m, 649197923531967450704711664m,  75809334407291469990832437230m, 78322691297526401047122740223m, 4348529951871323093202439165m, 4990460191572192980035045640m, 5597312470813537077508379404m, 4980755617409140165251173636m, 1890741055734852330174483975m, 76772801025035254361275759599m,75502243563200070682362835182m, 78896921543467230670583692029m, 2489164206166677455700101373m, 4338830174078735659125311481m, 4960199192571758553533648130m, 3420013420025511569771334658m, 1557077491473974933188251927m, 77376040767919248347203368440m,  73949978050619586491881614568m, 77043619187199676893167803647m, 1212557245150259869494540530m, 3081561358716686153294085872m, 3392217589357453836837847030m, 1219782446916489227407330320m, 78580145051212187267589731866m, 75798434925965430405537592305m,68369566912511282590874449920m, 72396532057599326246617936384m, 75186737388538008131054524416m, 77027917484951889231108827392m, 73655004947793353634062267392m, 76417372019396591550492896512m, 74568981255592060493492515584m, 70529879645288096380279255040m}
             .Select(packedTable =>
             {
                 piece = 0;
                 return new System.Numerics.BigInteger(packedTable).ToByteArray().Take(12)
-                        .Select(square => (int)((sbyte)square * 1.461) + PieceValues[piece++])
+                        .Select(square => (int)((sbyte)square * 1.461) + new[] {82, 337, 365, 477, 1025, 0,94, 281, 297, 512, 936, 0}[piece++]) 
                     .ToArray();
             }).ToArray();
-        }   
+         
 
         Move rootBestMove;
         readonly Move[] kMoves = new Move[2048];
@@ -43,9 +39,9 @@ using System.Linq;
                 // Eval function
                 int staticEvalPos()
                 {
-                    int middlegame = 0, endgame = 0, gamephase = 0, sideToMove = 2, square;
+                    int middlegame = 0, endgame = 0, sideToMove = 2, square, gamephase = 0;
                     for (; --sideToMove >= 0; middlegame *= -1, endgame *= -1   )
-                        for (piece = -1; ++piece < 6;)
+                        for (piece = -1 ; ++piece < 6;)
                             for (ulong mask = board.GetPieceBitboard((PieceType)piece + 1, sideToMove > 0); mask != 0;)
                             {
                                      
@@ -64,13 +60,13 @@ using System.Linq;
                 // Search function
                 int Negamax(int depth, int alpha, int beta, int ply, bool notLastMoveNull) {
 
-                    bool isNotRoot = ply++ > 0, InCheck = board.IsInCheck(), notPvNode = alpha + 1 == beta, fprune = false;
+                    bool isNotRoot = ply++ > 0, InCheck = board.IsInCheck(), notPvNode = alpha + 1 == beta, fprune = false, qsearch;
                     ulong key = board.ZobristKey;
 
                     // Check for repetition
                     if(isNotRoot && board.IsRepeatedPosition()) return 0;   
 
-                    int score, bestScore = -6000001;
+                    int score, bestScore = -6000001, flag = 1, i = 0;
                     // Local function
                     int Search(int nextAlpha, int Reduction = 1, bool canNull = true) => score = -Negamax(depth - Reduction, -nextAlpha, -alpha, ply, canNull);
 
@@ -91,20 +87,20 @@ using System.Linq;
                     // Internal Iterative Reductions (IIR)
                     else if(depth > 4) depth--;
 
-                    bool qsearch = depth <= 0;
-                    if(qsearch) {
+                    
+                    if(qsearch = depth <= 0) {
                         bestScore = staticEvalPos();
                         if(bestScore >= beta) return bestScore; 
                         alpha = Math.Max(alpha, bestScore);
                     } else if (!InCheck && notPvNode) {
 
-                        if (depth <= 8) {                        
-                            int staticEval = staticEvalPos();
-                            if (staticEval - 100 * depth >= beta) return staticEval;
+                        int staticEval = staticEvalPos();
+                        if (depth <= 8) {
+                            if (staticEval - 75 * depth >= beta) return staticEval;
                             fprune = staticEval + 140 * depth <= alpha; 
                         }
     
-                        if (notLastMoveNull && depth >= 2) {
+                        if (staticEval >= beta && notLastMoveNull && depth >= 2) {
                             board.TrySkipTurn();
                             Search(beta, 3 + depth / 5, false);
                             board.UndoSkipTurn();
@@ -114,14 +110,13 @@ using System.Linq;
                     
                     Span<Move> allMoves = stackalloc Move[218];
                     board.GetLegalMovesNonAlloc(ref allMoves, qsearch);
-                    int flag = 1, i = 0;
             
                     // Move ordering
                     for(; i <  allMoves.Length;) {
                         Move move = allMoves[i];
                         scores[i++] = -(ttMove == move && ttKey == key ? 1000000000 :
                                         move.IsCapture ?  100000 * (int)move.CapturePieceType - (int)move.MovePieceType :
-                                        move.IsPromotion ? 100000 * (int)move.PromotionPieceType :  
+                                        move.IsPromotion ? 96000 * (int)move.PromotionPieceType :  
                                         kMoves[ply] == move ? 95000 :
                                         hMoves[ply & 1, (int)move.MovePieceType, move.TargetSquare.Index]);
                     }
@@ -136,7 +131,7 @@ using System.Linq;
                         if (timer.MillisecondsElapsedThisTurn * 30 >= timer.MillisecondsRemaining) depth /= 0;
 
                         // Futility pruning
-                        if (fprune && i != 0 && scores[i] > -100000) continue;
+                        if (fprune && i != 0 && scores[i] > -96000) continue;
                         if (notPvNode && bestScore <= 20000 && depth <= 6 && i > 2 + depth * depth && scores[i] > -95000) break;   
 
                         board.MakeMove(move);
